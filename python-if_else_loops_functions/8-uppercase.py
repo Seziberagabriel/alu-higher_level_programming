032ab885b69b70aae1d9eedd613cf48c8e1e1676
Seziberagabriel <g.tuyisingi@alustudent.com>
2024-10-21 16:13:14 +0200

    convert lower case str to upper case str

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-
 def uppercase(str):
-    for c in str:
-        if ord(c) >= 97 and ord(c) <= 122:
-            print(chr(ord(c) - 32), end="")
-        else:
-            print(c, end="")
+    for iterator in str:
+        temp = iterator
+        if ord(temp) >= 97 and ord(temp) <= 122:
+            temp = chr(ord(iterator) - 32)
+        print("{}".format(temp), end='')
     print()
-
-
-if __name__ == "__main__":
-    uppercase("best")
-    uppercase("Best School 98 Battery street")
