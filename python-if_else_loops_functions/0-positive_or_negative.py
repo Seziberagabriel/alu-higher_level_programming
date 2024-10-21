23838e4e7cd098d3ce8b66c66083861f00061b57
Seziberagabriel <g.tuyisingi@alustudent.com>
2024-10-21 12:43:43 +0200

    Task 0

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 
-number = random.randint(-10, 10)  # This generates a random signed number between -10 and 10
+number = random.randint(-10, 10)  # Generates a random signed number
 
 if number > 0:
     print(f"{number} is positive")
