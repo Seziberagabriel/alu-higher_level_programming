90cec9e98c6c63f2c8efd1a63a81393067a24860
Seziberagabriel <g.tuyisingi@alustudent.com>
2024-10-29 18:43:25 +0200

    Task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -7,13 +7,13 @@ def safe_print_list_integers(my_list=[], x=0):
                 print("{:d}".format(my_list[i]), end="")
                 count += 1
         except IndexError:
-            break
-    print()
+            raise IndexError("list index out of range")
+    print()  # Move to the next line after printing integers
     return count
 
 if __name__ == "__main__":
     my_list = [1, 2, 'a', 3, 4, 'b', 5]
     nb_print = safe_print_list_integers(my_list, 5)
     print(f"nb_print: {nb_print}")
-    nb_print = safe_print_list_integers(my_list, 7)
+    nb_print = safe_print_list_integers(my_list, len(my_list) + 2)
     print(f"nb_print: {nb_print}")
