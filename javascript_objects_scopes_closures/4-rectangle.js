735fa717b3327c78b1c264065c10a35e63684b25
Seziberagabriel <g.tuyisingi@alustudent.com>
2025-01-28 09:30:58 +0200

    Task 4

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,8 +1,31 @@
 #!/usr/bin/node
 
-function createRectangle(length, width) {
-  return {
-    length: length,
-    width: width
-  };
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      this.width = 0;
+      this.height = 0;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print() {
+    if (this.width === 0 || this.height === 0) return;
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate() {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
+
+module.exports = Rectangle;
